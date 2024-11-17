7dc9ce823e89b9c65495b56d860d1b24b1cdd130
LeonMateoCaceres <125699913+LeonMateoCaceres@users.noreply.github.com>
2024-11-17 02:19:36 -0300

    Add files via upload

---
+++ Control_De_Inventario/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_De_Inventario
+{
+    public class Productos
+    {
+        string Nombre { get; set; }
+        decimal Precio { get; set; }
+        int Cantidad { get; set; }
+        decimal Total {  get; set; }
+
+        public Productos (string nombre, decimal precio, int cantidad, decimal total)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+            Total = total;
+        }
+    }
+}
