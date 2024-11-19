25f9b1ffa5f524ad603283a02a39ee0fa596d30c
LeonMateoCaceres <125699913+LeonMateoCaceres@users.noreply.github.com>
2024-11-19 13:46:29 -0300

    Add files via upload

---
+++ Agenda_De_Contactos/Contactos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_De_Contactos
+{
+    public class Contactos
+    {
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Correo {  get; set; }
+
+        public Contactos (string nombre, int telefono, string correo)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+            Correo = correo;
+        }
+    }
+}
